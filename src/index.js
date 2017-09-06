4ada9b0e5c341d7cc59edec54109f1f95931b9fc
Ismael Perez <ismaelperezemail@gmail.com>
2017-09-05 17:38:31 -0700

    section 1 lecture 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyDutuO-kn_vZhs1CwThiNSishCD0_WUfnQ'
+
+
 // Create a new component. This component should produce
 // some HTML
 const App =  () => {
-  return <div>Hello React World!</div>;
+  return  (
+  <div>
+    <SearchBar />
+  </div>
+  );
 }
 
-// Take this component's generated HTML and put it on the page (in the DOM)
+// Take this component's generated HTML and put it
+//on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
